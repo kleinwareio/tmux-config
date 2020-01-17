42607501066efeeee408724594165256ecb2a3b1
Matt Klein <32728112+kleinwareio@users.noreply.github.com>
2020-01-16 22:20:39 -0700

    Adding CPU plugin

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-ln -s `dirname "$(readlink -f "$0")"`/.tmux.conf ~/.tmux.conf
+ln -s `dirname "$(readlink -f "$0")"`/tmux.conf ~/.tmux.conf
 
 tmux source ~/.tmux.conf
