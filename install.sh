4c9b80b8b4c322a67d657b9982a6bced87537ca0
Matt Klein <32728112+kleinwareio@users.noreply.github.com>
2025-05-28 20:18:37 +0000

    Fixing restore

--- install.sh
+++ install.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+# Clone TPM if it doesn't exist
+if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
+  git clone https://github.com/tmux-plugins/tpm "$HOME/.tmux/plugins/tpm"
+fi
 
-ln -s `dirname "$(readlink -f "$0")"`/tmux.conf ~/.tmux.conf
+# Link tmux.conf
+ln -sf "$(dirname "$(readlink -f "$0")")/tmux.conf" "$HOME/.tmux.conf"
 
-tmux source ~/.tmux.conf
+# Source tmux.conf
+tmux source-file "$HOME/.tmux.conf" 2>/dev/null || true
+
+# Install plugins
+"$HOME/.tmux/plugins/tpm/bin/install_plugins"
+
+echo "Tmux configuration installed successfully!"
